ab63b4d68e4e30dc073e02c3b1ed1b8710ad764b
arnoldswattasnugget <54468630+fishhater269@users.noreply.github.com>
2022-08-09 16:16:22 -0700

    Update bar-chart.R

--- bar-chart.R
+++ bar-chart.R
@@ -2,32 +2,35 @@ library(plotly)
 library(stringr)
 library(dplyr)
 
-twitch_df <- read.csv('data.csv')
+twitch_df <- read.csv("twitch.csv")
 
-gettotalviews <- function(yr) {
 
-  peakviews_df <- twitch_df %>% filter(str_detect(twitch_df$Month, yr))
-  #print(max(peakviews_df$Peak.viewers))
-  result <- (signif(max(peakviews_df$Peak.viewers) / 1000000, 3))
-  return(result)
+pv2016_df <- twitch_df[twitch_df$Year == 2016,]
+pv2017_df <- twitch_df[twitch_df$Year == 2017,]
+pv2018_df <- twitch_df[twitch_df$Year == 2018,]
+pv2019_df <- twitch_df[twitch_df$Year == 2019,]
+pv2020_df <- twitch_df[twitch_df$Year == 2020,]
+pv2021_df <- twitch_df[twitch_df$Year == 2021,]
+pv2022_df <- twitch_df[twitch_df$Year == 2022,]
 
-}
+pv2016 <- signif(max(pv2016_df$Peak.viewers)/1000000, 3)
+pv2017 <- signif(max(pv2017_df$Peak.viewers)/1000000, 3)
+pv2018 <- signif(max(pv2018_df$Peak.viewers)/1000000, 3)
+pv2019 <- signif(max(pv2019_df$Peak.viewers)/1000000, 3)
+pv2020 <- signif(max(pv2021_df$Peak.viewers)/1000000, 3)
+pv2021 <- signif(max(pv2021_df$Peak.viewers)/1000000, 3)
+pv2022 <- signif(max(pv2022_df$Peak.viewers)/1000000, 3)
 
-year <- c("2016", "2017", "2018", "2019", "2020", "2021", "2022" )
+years = c(2016, 2017, 2018, 2019, 2020, 2021, 2022)
+peakviews = c(pv2016, pv2017, pv2018, pv2019, pv2020, pv2021, pv2022) 
+                    
+barplot(peakviews, years,
+        main = "Annual Peak Viewership on Twitch",
+        xlab = "Year",
+        ylab = "Viewers(Millions)",
+        names.arg = years,
+        col = "purple",
+        horiz = "false")
 
-peakviews <- c(gettotalviews("2016"),
-               gettotalviews("2017"), gettotalviews("2018"),
-               gettotalviews("2019"), gettotalviews("2020"),
-               gettotalviews("2021"), gettotalviews("2022"))
 
-annualviewers_df <- data.frame(year, peakviews)
-
-bar_chart <- plot_ly(annualviewers_df,
-                     x = ~year,
-                     y = ~peakviews, 
-                     type = 'bar')
-
-bar_chart <- bar_chart %>% layout( title = "Peak Viewers for each Year on Twitch",
-                                   xaxis = list(title = "Year"),
-                                   yaxis = list(title = "Viewers (millions)"))
 
